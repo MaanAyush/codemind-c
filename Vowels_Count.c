63b62ecbba967d9295eb13326b04e878ebe50e20
MaanAyush <147319716+MaanAyush@users.noreply.github.com>
2024-01-03 00:21:54 +0530

    #include<stdio.h> #include<string.h> int main(){     char str[100];     scanf("%[^ ]s",str);     int c=0;     for(int i=0;i<=strlen(str);i++){         if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){             c++;         }     }printf("%d",c); }

---
+++ Vowels_Count.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    scanf("%[^
+]s",str);
+    int c=0;
+    for(int i=0;i<=strlen(str);i++){
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
+            c++;
+        }
+    }printf("%d",c);
+}
